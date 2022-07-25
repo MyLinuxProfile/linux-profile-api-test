bae169b3c4d10ecd123e71ded49119ff4f04a158
FernandoCelmer <email@fernandocelmer.com>
2022-07-24 22:21:24 -0300

    No Task - Delete user endpoints

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,10 +1,2 @@
 from fastapi import APIRouter
-from fastapi.responses import Response
-
 router = APIRouter()
-
-@router.get("/users")
-def read_users():
-    """Read Users
-    """
-    return Response(status_code=401)
