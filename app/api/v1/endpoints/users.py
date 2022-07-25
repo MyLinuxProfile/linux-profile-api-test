daea8391055b384fc79e926c49de93f815da6e4b
FernandoCelmer <email@fernandocelmer.com>
2022-07-24 22:21:03 -0300

    No Task - Delete user endpoints

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,35 +1,10 @@
-from fastapi import APIRouter, Request, Depends
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import Response, JSONResponse
-
-from typing import List
-
-from sqlalchemy.orm import Session
-
-from app.core.mysql import ControllerUser
-from app.schemas.user import SchemaUser
-
-from app.api.utils import validate_token
-from app.database.mysql import get_db
-from app.database.nosql import db
-from upy_error import format_exception
+from fastapi import APIRouter
+from fastapi.responses import Response
 
 router = APIRouter()
 
-@router.get("/users", response_model=List[SchemaUser])
-def read_users(request: Request, db: Session = Depends(get_db)):
+@router.get("/users")
+def read_users():
     """Read Users
     """
-    authorized = validate_token(request, db)
-
-    if type(authorized) == Response:
-        return authorized
-
-    try:
-        item = ControllerUser(db=db).get_all()
-        json_compatible_item_data = jsonable_encoder(item)
-
-        return JSONResponse(content=json_compatible_item_data)
-
-    except Exception as error:
-        print(format_exception(error))
+    return Response(status_code=401)
